0e0350f203a556cf23200ba9ac5ce69ce01f34a2
Rob Westgeest <rob.westgeest@gmail.com>
2012-05-05 21:45:54 +0200

    rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tafelmanager2::Application.config.secret_token = 'd75c28805704194676dae27eed5f4acdd2e0f038b9c5392290de85ac3d0d3d6c616fb08c5fce4337abe0263129257909fd23efa49cf0d162df62976623ce9a29'
