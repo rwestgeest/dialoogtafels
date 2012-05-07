5828eb1d3d92b8ad858d0f0b9d4e5ed1ef153c1a
Rob Westgeest <rob.westgeest@gmail.com>
2012-05-07 09:13:52 +0200

    updated views for tenants

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def title(title)
+    content_for(:title) { title }
+  end
+
+  def page_info(page_info)
+    content_for(:page_info, page_info)
+  end
+
+  def page_side_bar(side_bar)
+    content_for(:page_side_bar, side_bar)
+  end
+
+  def selected_text(text)
+    content_tag :span, text, :class => 'selected-text'
+  end
+
+  def parent_layout(layout)
+    @_content_for[:layout] = self.output_buffer
+    self.output_buffer = render(:file => "layouts/#{layout}")
+  end
+
+  def flash_messages
+    raw flash.collect { |key, message| content_tag(:div, message, :id => key) }.join($/)
+  end
+
 end
