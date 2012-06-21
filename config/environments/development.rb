3055e6c4cd1b3c1545283d937ffeb191ca425335
Rob Westgeest <rob.westgeest@gmail.com>
2012-06-21 15:24:22 +0200

    added html5

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,8 +31,18 @@ Tafelmanager2::Application.configure do
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # Do not compress assets
-  config.assets.compress = false
+  # config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  # config.assets.debug = true
+
+  #uncomment to simulate production behavior
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
+  # Don't fallback to assets pipeline if a precompiled asset is missed
+  config.assets.compile = false
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
 end
