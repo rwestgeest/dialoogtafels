1c943cd891470bdfc0ccd67156fe1d066028ab28
Rob Westgeest <rob@qwan.it>
2012-08-30 15:37:12 +0200

    fixed problem in creating people in forms

--- app/models/profile_field_value.rb
+++ app/models/profile_field_value.rb
@@ -1,7 +1,7 @@
 class ProfileFieldValue < ActiveRecord::Base
   attr_accessible :profile_field_id, :person_id, :value, :profile_field
   belongs_to :profile_field
-  belongs_to :person
+  belongs_to :person, inverse_of: :profile_field_values
   include ScopedModel
   scope_to_tenant
 
