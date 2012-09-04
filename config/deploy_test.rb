634c08ae0e3ea3f55340f673cf1673d4ebe21cae
Rob Westgeest <rob@qwan.it>
2012-09-04 10:15:40 +0200

    excluded migration specs

--- config/deploy_test.rb
+++ config/deploy_test.rb
@@ -1,6 +1,6 @@
 set :deploy_to, "/var/www/rails/#{application}-gator-test"
 set :domain, "#{user}@test.gator.#{application}.nl"
-set :rails_env, "test"
+set :rails_env, "development"
 set :passenger_port, 3032
 
 namespace :vlad do
@@ -15,7 +15,7 @@ namespace :vlad do
 
   remote_task "tests", :role => :app do
     in_current_path = "cd #{current_path} && "
-    run in_current_path + "RAILS_ENV=#{rails_env} bundle exec rake "
+    run in_current_path + "bundle exec rake "
   end
 
 end
