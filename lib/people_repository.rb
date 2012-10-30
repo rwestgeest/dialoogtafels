4fbc018bd5222319426323c47653438bbd205acf
Rob Westgeest <rob@qwan.it>
2012-10-30 13:40:23 +0100

    updated people repository

--- lib/people_repository.rb
+++ lib/people_repository.rb
@@ -1,17 +1,45 @@
-class PeopleRepository < Struct.new(:project)
+class PeopleRepository < Struct.new(:people_repository)
+  def for_date_range(start_date, end_date)
+    DateRangedPeopleRepository.new(self, start_date, end_date)
+  end
+
   def coordinators
     TenantAccount.coordinators
   end 
 
   def organizers
-    project.organizers
+    people_repository.organizers
   end
 
   def conversation_leaders
-    project.conversation_leaders
+    people_repository.conversation_leaders
   end
 
   def participants
-    project.participants
+    people_repository.participants
+  end
+end
+
+class DateRangedPeopleRepository < PeopleRepository 
+  def initialize(people_repository, start_date, end_date)
+    super(people_repository)
+    @start_date = start_date
+    @end_date = end_date
+  end 
+  def organizers
+    in_range(super.includes(:locations => :conversations))
+  end
+  def conversation_leaders
+    in_range(super.includes(:conversation))
+  end
+  def participants
+    in_range(super.includes(:conversation))
+  end
+
+  private
+  def in_range(relation)
+    relation
+    .where('conversations.start_time >= ?', @start_date)
+    .where('conversations.start_time <= ?', @end_date.tomorrow)
   end
 end
--- spec/lib/people_repository_spec.rb
+++ spec/lib/people_repository_spec.rb
@@ -1,21 +1,60 @@
 require 'spec_helper'
 require 'people_repository'
 
+class DateRange  < Struct.new(:start_date, :end_date)
+end
+
 describe PeopleRepository do
   prepare_scope :tenant
   let(:current_tenant) { Tenant.current } 
   let(:project)    { current_tenant.active_project }
-  let(:subject) { PeopleRepository.new(project) }
-  
-  let(:coordinator) { FactoryGirl.create :coordinator_account }  
-  its(:coordinators) { should include(coordinator) } 
+  let!(:conversation) { FactoryGirl.create :conversation } 
+  let!(:location) { conversation.location }
+  let!(:coordinator) { FactoryGirl.create :coordinator_account }  
+  let!(:organizer) { location.organizer } 
+  let!(:conversation_leader) { FactoryGirl.create :conversation_leader, conversation: conversation }
+  let!(:participant) { FactoryGirl.create :participant, conversation: conversation }
 
-  let(:organizer) { FactoryGirl.create :organizer }
-  its(:organizers) { should == [ organizer ] }
+  context "without date range" do
+    let(:subject) { PeopleRepository.new(project) }
 
-  let(:conversation_leader) { FactoryGirl.create :conversation_leader }
-  its(:conversation_leaders) { should == [ conversation_leader ] }
+    its(:coordinators) { should include(coordinator) } 
 
-  let(:participant) { FactoryGirl.create :participant }
-  its(:participants) { should == [ participant ] }
+    its(:organizers) { should == [ organizer ] }
+
+    its(:conversation_leaders) { should == [ conversation_leader ] }
+
+    its(:participants) { should == [ participant ] }
+  end
+
+  context "with date range" do
+    let(:start_date) { Date.today }
+    let(:end_date) { Date.today }
+    let(:subject) { PeopleRepository.new(project).for_date_range(start_date, end_date) }
+
+    context "when conversation falls before the date_range" do
+      before { conversation.update_attribute :start_date, start_date.yesterday } 
+      its(:coordinators) { should include(coordinator) } 
+      its(:organizers)   { should == [] }
+      its(:conversation_leaders)   { should == [] }
+      its(:participants)   { should == [] }
+    end
+
+    context "when conversation falls after the date_range" do
+      before { conversation.update_attribute :start_date, end_date.tomorrow } 
+      its(:coordinators) { should include(coordinator) } 
+      its(:organizers)   { should == [] }
+      its(:conversation_leaders)   { should == [] }
+      its(:participants)   { should == [] }
+    end
+
+    context "when conversation is within the date_range" do
+      before { conversation.update_attribute :start_date, start_date } 
+      its(:coordinators) { should include(coordinator) } 
+      its(:organizers)   { should == [organizer] }
+      its(:conversation_leaders)   { should == [conversation_leader] }
+      its(:participants)   { should == [participant] }
+    end
+  end
+
 end
